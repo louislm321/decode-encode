f06c8dd2719d5205ea82ef6aa18ae3677f66a95f
louis <louislm321@gmail.com>
2023-10-31 18:49:44 -0400

    second edition

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-#this is my first github edition
+# louis lee
 
-print('hello earth')
+def printMenu():
+    print("\nMenu\n"
+          "-------------\n"
+          "1. Encode\n"
+          "2. Decode\n"
+          "3. Quit\n")
 
 
+def encode(num):
+    encodedPassword = ""
+    for digit in num:
+        digitShift = str((int(digit) + 3) % 10)
+        encodedPassword += digitShift
+
+    return encodedPassword
+
+
+def main():
+    MenuOn = True
+    while MenuOn:
+        printMenu()
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = str(input("Please enter your password to encode: "))
+            encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            print("The encoded password is " + encode(password) + ", and the original password is " + password + ".")
+        else:
+            MenuOn = False
+
+
+if __name__ == "__main__":
+    main()
